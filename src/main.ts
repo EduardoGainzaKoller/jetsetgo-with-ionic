f9488406f64062355de10d502cd6856c284bb341
eduardogainzakoller <edaniel2004gainza@gmail.com>
2025-05-07 13:05:58 +0100

    Register page completed

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "jetsetgo-with-ionic", appId: "1:384823113336:web:3ae2d9afe70d23f068df8d", storageBucket: "jetsetgo-with-ionic.firebasestorage.app", apiKey: "AIzaSyDiFxSjZDYgPOfC5KiRCVu-UH44f9RCRvY", authDomain: "jetsetgo-with-ionic.firebaseapp.com", messagingSenderId: "384823113336" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()),
   ],
 });
